c993d6ba0b2d29decec9c00be7f550d448aee492
elia <eliapinmor@monlau.com>
2025-11-26 19:18:53 +0100

    feedback dependiendo del score

--- src/components/GameEnd.vue
+++ src/components/GameEnd.vue
@@ -1,9 +1,23 @@
 <template>
-    <p>juego terminado</p>
-    <p>Tu puntaje final es: {{ quizStore.score }}</p>
-    
-    </template>
+  <p>juego terminado</p>
+  <p>Tu puntaje final es: {{ quizStore.score }}</p>
+  <div v-if="quizStore.score == 100">
+    <p>¡Lo has hecho fantástico!</p>
+  </div>
+  <div v-else-if="quizStore.score >= 70">
+    <p>¡Buen trabajo!</p>
+  </div>
+  <div v-else-if="quizStore.score >= 50">
+    <p>¡Puedes hacerlo mejor!</p>
+  </div>
+  <div v-else>
+    <p>¡Sigue practicando!</p>
+  </div>
+  <button @click="quizStore.startGame()">Volver a jugar</button>
+  <ButtonGame route="/ranking" content="Ver el Ranking" />
+</template>
 <script setup>
+import ButtonGame from "@/components/ButtonGame.vue";
 import { useQuizStore } from "@/stores/quizStore";
 const quizStore = useQuizStore();
 </script>
